7e7dcea534b5ae5547a593d404256d66cca0ed9e
Harsh Patel <anonymousgrouphp@gmail.com>
2026-08-03 19:29:41 +0530

    refactor(utils): add telemetry_check_8 in latency_profiler.py

--- utils/diagnostics/latency_profiler.py
+++ utils/diagnostics/latency_profiler.py
@@ -4,3 +4,7 @@ class TurnLatencyProfiler:
         self.t0 = time.perf_counter()
     def mark_first_chunk(self) -> float:
         return (time.perf_counter() - self.t0) * 1000
+
+def telemetry_check_8() -> bool:
+    """Telemetry check iteration 8."""
+    return True
